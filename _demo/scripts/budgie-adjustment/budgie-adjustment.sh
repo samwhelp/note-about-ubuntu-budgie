3c499a05c592b04785677805af62d6f5780be386
samwhelp <samwhelp@users.noreply.github.com>
2025-01-24 06:03:46 +0000

    fix

--- _demo/scripts/budgie-adjustment/budgie-adjustment.sh
+++ _demo/scripts/budgie-adjustment/budgie-adjustment.sh
@@ -330,6 +330,13 @@ mod_budgie_config_for_system_config_leave () {
 mod_tool_tilix_config () {
 
 
+	gsettings set com.gexperts.Tilix.Keybindings terminal-zoom-normal '<Ctrl>0'
+
+	gsettings set com.gexperts.Tilix.Keybindings terminal-zoom-out '<Ctrl>minus'
+
+	gsettings set com.gexperts.Tilix.Keybindings terminal-zoom-in '<Ctrl>equal'
+
+
 	return 0
 }
 
@@ -382,6 +389,53 @@ mod_tool_nemo_config () {
 mod_tool_gedit_config () {
 
 
+	gsettings set org.gnome.gedit.preferences.editor auto-indent true
+
+	gsettings set org.gnome.gedit.preferences.editor auto-save false
+
+	gsettings set org.gnome.gedit.preferences.editor auto-save-interval 10
+
+	gsettings set org.gnome.gedit.preferences.editor background-pattern 'none'
+
+	gsettings set org.gnome.gedit.preferences.editor bracket-matching true
+
+	gsettings set org.gnome.gedit.preferences.editor create-backup-copy false
+
+	gsettings set org.gnome.gedit.preferences.editor display-line-numbers true
+
+	gsettings set org.gnome.gedit.preferences.editor display-right-margin true
+
+	gsettings set org.gnome.gedit.preferences.editor editor-font 'Monospace 14'
+
+	gsettings set org.gnome.gedit.preferences.editor ensure-trailing-newline true
+
+	gsettings set org.gnome.gedit.preferences.editor highlight-current-line true
+
+	gsettings set org.gnome.gedit.preferences.editor insert-spaces false
+
+	gsettings set org.gnome.gedit.preferences.editor max-undo-actions 2000
+
+	gsettings set org.gnome.gedit.preferences.editor restore-cursor-position true
+
+	gsettings set org.gnome.gedit.preferences.editor right-margin-position 80
+
+	gsettings set org.gnome.gedit.preferences.editor scheme 'oblivion'
+
+	gsettings set org.gnome.gedit.preferences.editor search-highlighting true
+
+	gsettings set org.gnome.gedit.preferences.editor smart-home-end 'after'
+
+	gsettings set org.gnome.gedit.preferences.editor syntax-highlighting true
+
+	gsettings set org.gnome.gedit.preferences.editor tabs-size 4
+
+	gsettings set org.gnome.gedit.preferences.editor use-default-font false
+
+	gsettings set org.gnome.gedit.preferences.editor wrap-last-split-mode 'word'
+
+	gsettings set org.gnome.gedit.preferences.editor wrap-mode 'word'
+
+
 	return 0
 }
 
@@ -422,7 +476,7 @@ mod_budgie_config () {
 
 mod_tool_config () {
 
-	mod_tool_tilix_config
+	#mod_tool_tilix_config
 
 	mod_tool_nemo_config
 
